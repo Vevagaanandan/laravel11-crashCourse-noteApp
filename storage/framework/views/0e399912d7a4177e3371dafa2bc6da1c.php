7206e0a8f4d753e30404a88f32eb0f447bb73d69
Vevagaanandan Pillai <75841061+Vevagaanandan@users.noreply.github.com>
2024-09-26 20:10:20 +0800

    Add files via upload

---
+++ storage/framework/views/0e399912d7a4177e3371dafa2bc6da1c.php
@@ -0,0 +1,48 @@
+<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54 = $attributes; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\AppLayout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <div class="note-container py-12">
+        <a href="<?php echo e(route('note.create')); ?>" class="new-note-btn">New Note</a>
+        <div class="notes">
+            <?php $__currentLoopData = $notes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $note): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <div class="note">
+                    <div class="note-body">
+                        <?php echo e(Str::words($note->note, 30)); ?>
+
+                    </div>
+                    <div class="note-buttons">
+                        <a href="<?php echo e(route('note.show', $note)); ?>" class="note-edit-button">View</a>
+                        <a href="<?php echo e(route('note.edit', $note)); ?>" class="note-edit-button">Edit</a>
+                        <form action="<?php echo e(route('note.destroy', $note)); ?>" method="POST">
+                            <?php echo csrf_field(); ?>
+                            <?php echo method_field('DELETE'); ?>
+                            <button class="note-delete-button">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+
+        <div class="p-6">
+            <?php echo e($notes->links()); ?>
+
+        </div>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $attributes = $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php /**PATH C:\Users\tubev\laravel11-crash-course\resources\views/note/index.blade.php ENDPATH**/ ?>
